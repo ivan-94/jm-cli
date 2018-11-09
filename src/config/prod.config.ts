07663f4cba043d735347fc6d14d4ec4a99be79c9
carney520 <carney520@hotmail.com>
2018-11-09 23:26:25 +0800

    压缩配置优化, 使用terser取代uglifyJS

--- src/config/prod.config.ts
+++ src/config/prod.config.ts
@@ -5,16 +5,19 @@ import path from 'path'
 import { WebpackConfigurer } from './type'
 
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const safePostCssParser = require('postcss-safe-parser')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 const configure: WebpackConfigurer = (enviroments, pkg, paths, argv) => {
   const { name } = argv
   const filePrefix = name ? `${name}_` : ''
   const shouldUseRelativeAssetPaths = enviroments.raw.PUBLIC_URL === './'
-  const cssFilename = `static/css/${filePrefix}[name].css?[hash]`
+  const shouldUseSourceMap = enviroments.raw.SOURCE_MAP !== 'false'
 
   return {
-    devtool: enviroments.raw.SOURCE_MAP === 'false' ? false : 'source-map',
+    devtool: shouldUseSourceMap && 'source-map',
+    bail: true,
     entry: {},
     module: {
       rules: [
@@ -24,7 +27,7 @@ const configure: WebpackConfigurer = (enviroments, pkg, paths, argv) => {
             {
               loader: MiniCssExtractPlugin.loader,
               options: {
-                publicPath: shouldUseRelativeAssetPaths ? Array(cssFilename.split('/').length).join('../') : undefined,
+                publicPath: shouldUseRelativeAssetPaths ? '../../' : undefined,
               },
             },
             {
@@ -52,19 +55,68 @@ const configure: WebpackConfigurer = (enviroments, pkg, paths, argv) => {
     plugins: [
       // 抽取CSS文件
       new MiniCssExtractPlugin({
-        filename: cssFilename,
+        filename: `static/css/${filePrefix}[name].css?[contenthash:8]`,
+        chunkFilename: `static/css/${filePrefix}[name].chunk.css?[contenthash:8]`,
       }),
     ],
     optimization: {
+      minimize: true,
       minimizer: [
-        new UglifyJsPlugin({
-          cache: true,
-          parallel: true,
-          sourceMap: true, // set to true if you want JS source maps
-          uglifyOptions: {
-            compress: {
-              warnings: false,
+        new TerserPlugin({
+          terserOptions: {
+            parse: {
+              // we want terser to parse ecma 8 code. However, we don't want it
+              // to apply any minfication steps that turns valid ecma 5 code
+              // into invalid ecma 5 code. This is why the 'compress' and 'output'
+              // sections only apply transformations that are ecma 5 safe
+              // https://github.com/facebook/create-react-app/pull/4234
+              ecma: 8,
             },
+            compress: {
+              ecma: 5,
+              warnings: false,
+              // Disabled because of an issue with Uglify breaking seemingly valid code:
+              // https://github.com/facebook/create-react-app/issues/2376
+              // Pending further investigation:
+              // https://github.com/mishoo/UglifyJS2/issues/2011
+              comparisons: false,
+              // Disabled because of an issue with Terser breaking valid code:
+              // https://github.com/facebook/create-react-app/issues/5250
+              // Pending futher investigation:
+              // https://github.com/terser-js/terser/issues/120
+              inline: 2,
+            },
+            mangle: {
+              safari10: true,
+            },
+            output: {
+              ecma: 5,
+              comments: false,
+              // Turned on because emoji and regex is not minified properly using default
+              // https://github.com/facebook/create-react-app/issues/2488
+              ascii_only: true,
+            },
+          },
+          // Use multi-process parallel running to improve the build speed
+          // Default number of concurrent runs: os.cpus().length - 1
+          parallel: true,
+          // Enable file caching
+          cache: true,
+          sourceMap: shouldUseSourceMap,
+        }),
+        new OptimizeCSSAssetsPlugin({
+          cssProcessorOptions: {
+            parser: safePostCssParser,
+            map: shouldUseSourceMap
+              ? {
+                  // `inline: false` forces the sourcemap to be output into a
+                  // separate file
+                  inline: false,
+                  // `annotation: true` appends the sourceMappingURL to the end of
+                  // the css file, helping the browser find the sourcemap
+                  annotation: true,
+                }
+              : false,
           },
         }),
       ],

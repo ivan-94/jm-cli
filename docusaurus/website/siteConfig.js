0cb8a451f3a54dcd9dfc46ef0a5c8a882872c130
carney520 <carney520@hotmail.com>
2018-11-16 06:49:30 +0800

    add configurations page

--- docusaurus/website/siteConfig.js
+++ docusaurus/website/siteConfig.js
@@ -23,8 +23,8 @@ const users = [
 const siteConfig = {
   title: 'JM-CLI', // Title for your website.
   tagline: 'CLI for run and build React + Typescript App',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://carney520.github.io', // Your website URL
+  baseUrl: '/jm-cli/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',

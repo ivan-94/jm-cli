1d547c33e866c6152c8ee6e86b316b3e936a5567
ivan-94 <carney520@hotmail.com>
2019-05-20 11:22:23 +0800

    refactor: 进行异步升级检查提升命令启动速度

--- src/middlewares/upgrade.ts
+++ src/middlewares/upgrade.ts
@@ -2,18 +2,16 @@
  * Check for updates regularly
  */
 import { Arguments } from 'yargs'
-import { execSync } from 'child_process'
 import boxen from 'boxen'
 import fs from 'fs-extra'
 import chalk from 'chalk'
-import inquirer from 'inquirer'
 import path from 'path'
 import Ora from 'ora'
 import { getYarnGlobalInstallPackages, getNpmGlobalInstallPackages, getUpgradableVersion } from '../services/upgrade'
 import paths from '../paths'
 import { shouldUseYarn, IS_CI } from '../utils'
 
-const Daily = 24 * 3600 * 1000
+const Weekly = 7 * 24 * 3600 * 1000
 let spinner = new Ora()
 
 /**
@@ -69,43 +67,25 @@ async function checkLocalUpdate(useYarn: boolean, pkg: { name: string; version: 
   spinner.text = 'Checking for update...'
   spinner.start()
   const { name, version } = pkg
-  const [newRange, upgradable, newMaxVersion] = getUpgradableVersion(name, version, 'minor')
+  const [, upgradable, newMaxVersion] = getUpgradableVersion(name, version, 'minor')
   if (upgradable) {
     showUpdateInfo(useYarn, name, version, newMaxVersion)
-    const { ok } = await inquirer.prompt<{ ok: boolean }>({
-      type: 'confirm',
-      message: 'update now?',
-      name: 'ok',
-      default: true,
-    })
-
-    if (ok) {
-      const cmd = useYarn ? `yarn add "${name}@${newRange}" -D` : `npm install "${name}@${newRange}" --save-dev`
-      spinner.start()
-      spinner.text = 'Upgrading...'
-      execSync(cmd, { stdio: ['ignore', 'ignore', 'inherit'] })
-      console.log(cmd)
-      spinner.succeed('Upgrade Success!')
-    }
   }
 }
 
-export default async (argv: Arguments) => {
-  if (IS_CI) {
-    return
-  }
-
+async function checkUpdate() {
   const lastUpdatePath = path.join(paths.ownData, 'lastupdate')
-  fs.ensureDirSync(paths.ownData)
+  await fs.ensureDir(paths.ownData)
 
   // 首次安装，暂不需要检查
   if (!fs.existsSync(lastUpdatePath)) {
-    fs.writeJSONSync(lastUpdatePath, Date.now())
+    fs.writeJSON(lastUpdatePath, Date.now())
     return
   }
-  const lastUpdate = fs.readJSONSync(lastUpdatePath) as number
 
-  if (Date.now() - lastUpdate < Daily) {
+  const lastUpdate = (await fs.readJSON(lastUpdatePath)) as number
+
+  if (Date.now() - lastUpdate < Weekly) {
     return
   }
 
@@ -123,4 +103,13 @@ export default async (argv: Arguments) => {
     fs.writeJSONSync(lastUpdatePath, Date.now())
     spinner.stop()
   }
+}
+
+export default async (argv: Arguments) => {
+  if (IS_CI) {
+    return
+  }
+
+  // 异步检查，不影响主流程
+  checkUpdate()
 }

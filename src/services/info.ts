6e1851cb49a229f21641335b8f1628146afdbc81
carney520 <carney520@hotmail.com>
2019-07-26 13:51:35 +0800

    feat: 展示jm-cli版本信息

--- src/services/info.ts
+++ src/services/info.ts
@@ -1,14 +1,20 @@
 import { readJsonSync } from 'fs-extra'
+import path from 'path'
 import { resolveModuleInCwd, isModuleExistsInCwd } from '../utils'
+
+const pkg = require(path.join(__dirname, '../../package.json'))
 
 export default function showInfo() {
   const modules = ['typescript', 'react', 'react-dom', 'electron']
-  return modules
-    .filter(isModuleExistsInCwd)
-    .map(name => {
-      const pkgPath = resolveModuleInCwd(`${name}/package.json`)
-      const pkg = readJsonSync(pkgPath)
-      return `${name}: ${pkg.version}`
-    })
-    .join(', ')
+  return (
+    `jm-cli: ${pkg.version} ` +
+    modules
+      .filter(isModuleExistsInCwd)
+      .map(name => {
+        const pkgPath = resolveModuleInCwd(`${name}/package.json`)
+        const pkg = readJsonSync(pkgPath)
+        return `${name}: ${pkg.version}`
+      })
+      .join(', ')
+  )
 }
